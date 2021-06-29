aa7e4df63b4783465b4e2d5686250c2455997b63
ifty64bit <19-40447-1@student.aiub.edu>
2021-06-29 23:37:57 +0600

    reg 3 4 php ifty

---
+++ registration3.php
@@ -0,0 +1,105 @@
+<?php 
+    $f_name="";
+    $f_name_error="";
+    $f_nid="";
+    $f_nid_error="";
+    $f_work_addr="";
+    $f_work_addr_error="";
+    $f_occupation="";
+    $f_occupation_error="";
+    $hasError=false;
+
+    if($_SERVER['REQUEST_METHOD']=='POST')
+    {
+        if(empty($_POST['f_name']))
+        {
+            $hasError=true;
+            $f_name_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $f_name=$_POST['f_name'];
+        }
+
+        if(empty($_POST['f_nid']))
+        {
+            $hasError=true;
+            $f_nid_error="Field Can Not Be Empty";
+        }
+        elseif(!is_numeric($_POST['f_nid'])){
+            $hasError=true;
+            $f_nid_error="Field Value Must BE Numaric";
+        }
+        else{
+            $f_nid=$_POST['f_nid'];
+        }
+
+        if(empty($_POST['f_work_addr']))
+        {
+            $hasError=true;
+            $f_work_addr_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $f_work_addr=$_POST['f_work_addr'];
+        }
+
+        if(!isset($_POST['f_occupation']))
+        {
+            $hasError=true;
+            $f_occupation_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $f_occupation=$_POST['f_occupation'];
+        }
+    }
+?>
+<html> 
+    <head> 
+        <title>Page Title</title>
+    </head>
+    <body> 
+        <form action="" method="post"> 
+            <fieldset>
+                <legend><h2>Parent's Info</h2></legend>
+                <table align="center">
+                    <tr> 
+                        <td>Father's Name:</td> 
+                        <td><input type="text" name="f_name" value="<?php echo $f_name ?>"></td>
+                        <td><?php echo $f_name_error ?></td>
+                    </tr>
+                    <tr>
+                        <td>Father's NID:</td> 
+                        <td><input type="text" name="f_nid" value="<?php echo $f_nid ?>"></td> 
+                        <td><?php echo $f_nid_error ?></td>
+                    </tr>
+                    <tr>
+                        <td>Father's work  Address:</td> 
+                        <td><input type="text" name="f_work_addr" value="<?php echo $f_work_addr ?>"></td> 
+                        <td><?php echo $f_work_addr_error ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Occupation:</td> 
+                        <td>
+                            <select name="f_occupation"> 
+                                <option value="" selected disabled>--Select--</option>
+                                <option <?php echo $f_occupation=='service'?"selected":"" ?> value="service">Service</option> 
+                                <option <?php echo $f_occupation=='busniess'?"selected":"" ?> value="busniess">Business</option>
+                                <option <?php echo $f_occupation=='teacher'?"selected":"" ?> value="teacher">Teacher</option>
+                            </select> 
+                        </td> 
+                        <td><?php echo $f_occupation_error ?></td>
+                    </tr>
+                    <tr> 
+                        <td> 
+                            <input type="submit" value="Next">
+                        </td> 
+                    </tr> 
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
+  
+	
---
+++ registration4.php
@@ -0,0 +1,105 @@
+<?php 
+    $m_name="";
+    $m_name_error="";
+    $m_nid="";
+    $m_nid_error="";
+    $m_work_addr="";
+    $m_work_addr_error="";
+    $m_occupation="";
+    $m_occupation_error="";
+    $hasError=false;
+
+    if($_SERVER['REQUEST_METHOD']=='POST')
+    {
+        if(empty($_POST['m_name']))
+        {
+            $hasError=true;
+            $m_name_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $m_name=$_POST['m_name'];
+        }
+
+        if(empty($_POST['m_nid']))
+        {
+            $hasError=true;
+            $m_nid_error="Field Can Not Be Empty";
+        }
+        elseif(!is_numeric($_POST['m_nid'])){
+            $hasError=true;
+            $m_nid_error="Field Value Must BE Numaric";
+        }
+        else{
+            $m_nid=$_POST['m_nid'];
+        }
+
+        if(empty($_POST['m_work_addr']))
+        {
+            $hasError=true;
+            $m_work_addr_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $m_work_addr=$_POST['m_work_addr'];
+        }
+
+        if(!isset($_POST['m_occupation']))
+        {
+            $hasError=true;
+            $m_occupation_error="Field Can Not Be Empty";
+        }
+
+        else{
+            $m_occupation=$_POST['m_occupation'];
+        }
+    }
+?>
+
+<html> 
+    <head> 
+        <title>Page Title</title>
+    </head>
+    <body> 
+        <form action="" method="POST"> 
+            <fieldset>
+                <legend><h2>Parent's Info</h2></legend>
+                <table align="center">
+                    <tr> 
+                        <td>Mother's Name:</td> 
+                        <td><input type="text" name="m_name" value="<?php echo $m_name ?>"></td> 
+                        <td><?php echo $m_name_error ?></td>
+                    </tr>
+                    <tr>
+                        <td>Mother's NID:</td> 
+                        <td><input type="text" name="m_nid" value="<?php echo $m_nid ?>"></td> 
+                        <td><?php echo $m_nid_error ?></td>
+                    </tr>
+                    <tr>
+                        <td>Mother's work  Address:</td> 
+                        <td><input type="text" name="m_work_addr" value="<?php echo $m_work_addr ?>"></td> 
+                        <td><?php echo $m_work_addr_error ?></td>
+                    </tr>
+                    <tr> 
+                        <td>Occupation:</td> 
+                        <td>
+                            <select name="m_occupation">
+                                <option value="----" selected disabled>--SELECT--</option>
+                                <option <?php echo $m_occupation=='service'?"selected":"" ?> value="service">Service</option> 
+                                <option <?php echo $m_occupation=='business'?"selected":"" ?> value="business">Business</option>
+                                <option <?php echo $m_occupation=='teacher'?"selected":"" ?> value="teacher">Teacher</option>
+                            </select> 
+                        </td> 
+                        <td><?php echo $m_occupation_error ?></td>
+                    </tr>
+                    <tr> 
+                        <td></td>
+                        <td> <input type="submit" value="Next"></td> 
+                    </tr> 
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
+  
+	
