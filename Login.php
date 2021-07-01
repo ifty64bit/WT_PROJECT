afc7216bed93790a9b25e12a8be3306054a46564
ifty64bit <19-40447-1@student.aiub.edu>
2021-07-02 04:56:35 +0600

    home and login

---
+++ Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <legend><h2>Login</h2></legend>
+            <table align="center">
+                <tr>
+                    <td>Email:</td>
+                    <td><input type="text" name="email"></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="text" name="password"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Log in"></td>
+                </tr>
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
